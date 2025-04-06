3896556b6b157e993b47d2bc8448f6a5419327bb
A P Prajwal <apprajwalwork@gmail.com>
2025-04-06 23:42:17 +0530

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,12 @@ const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://lets-call.onrender.com", // or use '*' for testing (not recommended for prod)
+    credentials: true, // if using cookies or auth headers
+  })
+);
 app.set("port", process.env.PORT || 8080);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
